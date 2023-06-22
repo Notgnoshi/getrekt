e4021167250f5ce9fc5150c563d560f63bafa2c9
Austin Gill <Notgnoshi@gmail.com>
2023-06-22 10:38:33 -0500

    Add getrekt namespace

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,24 @@
 #include <emscripten/bind.h>
 
+namespace getrekt {
 int area(int width, int height) { return width * height; }
 
-EMSCRIPTEN_BINDINGS(getrekt) { emscripten::function("rectangle_area", &area); }
+class Rectangle
+{
+  public:
+    Rectangle(int x, int y, int width, int height) noexcept :
+        m_x{x}, m_y{y}, m_width{width}, m_height{height}
+    {
+    }
+
+    int area() const noexcept { return m_width * m_height; }
+
+  private:
+    int m_x;
+    int m_y;
+    int m_width;
+    int m_height;
+};
+}  // namespace getrekt
+
+EMSCRIPTEN_BINDINGS(getrekt) { emscripten::function("rectangle_area", &getrekt::area); }
